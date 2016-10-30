67577f0aedd6a833d7fdae47d5d40ea7c80db921
mthomps4 <matthew.thompson.a@gmail.com>
2016-10-30 16:26:25 -0400

    Reset SQL Credentials.php

--- inc/credentials.php
+++ inc/credentials.php
@@ -1,6 +1,6 @@
 <?php
 $dbname = "3WireWish";//Leave set
-$dbuser = "3Wire"; //Enter Database Username here
-$dbpw = "RpGQBn3WcPWc2tXZ"; //Enter Database Password here
+$dbuser = ""; //Enter Database Username here
+$dbpw = ""; //Enter Database Password here
 
 ?>
